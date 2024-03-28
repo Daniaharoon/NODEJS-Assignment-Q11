a6da474f76a995472425c4f033ca39ca0062a6c5
Dania Haroon <159708973+Daniaharoon@users.noreply.github.com>
2024-03-28 10:20:19 +0500

    Add files via upload

---
+++ ques11.js
@@ -0,0 +1,7 @@
+"use strict";
+// / //  11. Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+let frnds = ["Maham", "Aliza", "Jaweriya"];
+console.log(frnds);
+console.log(frnds[0]);
+console.log(frnds[1]);
+console.log(frnds[2]);
